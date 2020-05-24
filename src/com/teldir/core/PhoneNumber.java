94919f7aeb478ccf61027c3fd27e3def1ece6901
Vladimir Joshkin <vladimirjoshkin@gmail.com>
2020-05-24 18:36:12 +0800

    number elements extraction methods added

--- src/com/teldir/core/PhoneNumber.java
+++ src/com/teldir/core/PhoneNumber.java
@@ -1,14 +1,29 @@
 package com.teldir.core;
 
+import java.util.regex.Pattern;
+
 public class PhoneNumber {
+    private int id;
     private Heading heading;
     private Owner owner;
     private String number;
 
-    public PhoneNumber(Heading heading, Owner owner, String number) {
+    /*  Number structure:
+        + (C)[1-3] ( (A)[3-4] ) (B)[6-8]
+        C - country code, variable length 1 - 3
+        A - area code, variable length 3 - 4
+        B - body, variable length 6 - 8
+    */
+
+    public PhoneNumber(int id, Heading heading, Owner owner, String number) {
+        this.id = id;
         this.heading = heading;
         this.owner = owner;
         this.number = number;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public Heading getHeading() {
@@ -21,5 +36,17 @@ public class PhoneNumber {
 
     public String getNumber() {
         return number;
+    }
+
+    public int getCountryCode() {
+        return Integer.parseInt(number.split(Pattern.quote("("))[0].split(Pattern.quote("+"))[1]);
+    }
+
+    public int getAreaCode() {
+        return Integer.parseInt(number.split(Pattern.quote("("))[1].split(Pattern.quote(")"))[0]);
+    }
+
+    public int getBody() {
+        return Integer.parseInt(number.split(Pattern.quote(")"))[1]);
     }
 }
